e3ca6634b2a2386790822bc38f95ba06f29e8942
Alex <edosa.ezekiel08@majorana.org>
2026-02-17 20:17:38 +0100

    Update script.js

--- Sites/script.js
+++ Sites/script.js
@@ -1,1 +1,26 @@
-alert("Hello");
+// crea la mappa centrata su Mexico City
+const map = L.map('map').setView([19.4326, -99.1332], 13);;
+const marker = L.marker([19.4326, -99.1332]).addTo(map);
+
+// aggiunge le tiles (la mappa visiva)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+marker.bindPopup("Questo è la citta del Messico, la capitale del Messico").openPopup();
+
+/*
+map.on('click', function(e) {
+    L.marker(e.latlng).addTo(map).bindPopup("Hai cliccato qui!").openPopup();
+});
+*/
+
+
+L.circle([45.46, 9.19], {
+    radius: 500,
+    color: 'red'
+}).addTo(map);
+
+map.on('click', function(e) {
+    console.log(e.latlng.lat, e.latlng.lng);
+});
